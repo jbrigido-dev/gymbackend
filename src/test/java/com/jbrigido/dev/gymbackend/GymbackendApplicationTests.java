15ca2e5cd2d09e6a1db8d54509e4db333e0f566c
Brigido <jbrigidoperez@outlook.com>
2026-04-15 12:22:41 -0600

    Adding dependencies

---
+++ src/test/java/com/jbrigido/dev/gymbackend/GymbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbrigido.dev.gymbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
